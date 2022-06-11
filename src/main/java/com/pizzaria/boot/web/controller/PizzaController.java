0c420e9add134bce0768b3bf957ba954c1118567
João Pedro faria <jpalmeida.faria@gmail.com>
2022-06-10 21:42:38 -0300

    refatorando projeto

---
+++ src/main/java/com/pizzaria/boot/web/controller/PizzaController.java
@@ -0,0 +1,20 @@
+package com.pizzaria.boot.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/departamentos")
+public class PizzaController {
+
+    @GetMapping("/cadastro")
+    public String cadastrar() {
+        return "/pizza/cadastro";
+    }
+
+    @GetMapping("/listar")
+    public String listar() {
+        return "/pizza/lista";
+    }
+}
